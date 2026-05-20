0db7ec6254c8940686d84cbd487ec062dea6aaba
shigelog146 <shigelog146@gmail.com>
2026-05-20 12:33:28 +0900

    Add files via upload

--- soba-BEJI-NEW/farmer/service-worker.js
+++ soba-BEJI-NEW/farmer/service-worker.js
@@ -1,47 +1,31 @@
-const CACHE_NAME = "vegetable-app-v1";
+const CACHE_NAME = "vegetable-app-v2";
 
-const urlsToCache = [
-  "./",
-  "./index.html",
-  "./list.html"
-];
+const urlsToCache = ["./", "./index.html", "./list.html"];
 
 // ===============================
 // インストール
 // ===============================
 self.addEventListener("install", (event) => {
-
   event.waitUntil(
-
-    caches.open(CACHE_NAME)
-      .then((cache) => {
-        return cache.addAll(urlsToCache);
-      })
-
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    }),
   );
-
 });
 
 // ===============================
 // キャッシュ優先読み込み
 // ===============================
 self.addEventListener("fetch", (event) => {
-
   event.respondWith(
+    caches.match(event.request).then((response) => {
+      // キャッシュがあれば返す
+      if (response) {
+        return response;
+      }
 
-    caches.match(event.request)
-      .then((response) => {
-
-        // キャッシュがあれば返す
-        if (response) {
-          return response;
-        }
-
-        // 無ければ通常通信
-        return fetch(event.request);
-
-      })
-
+      // 無ければ通常通信
+      return fetch(event.request);
+    }),
   );
-
 });
